cb90bfd9a3cef6afad023498219b91ae91bc45b9
Ismail ASSIL <ismailassil00@gmail.com>
2025-01-21 10:26:59 +0100

    format main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include "headers/Server.hpp"
 
-int main(int ac,char **av)
-{
-    if (parse_input(ac, av)) {
-        return (1);
-    }
-	Server server(av[1], av[2]);
+int main( int ac, char **av ) {
+	if ( parse_input( ac, av ) ) {
+		return ( 1 );
+	}
+	Server server( av[1], av[2] );
 	return 0;
 }
